df563c842162373820a1dce2051ccf6acd1c38ec
SabaSiva2916 <sabasiva2916@gmail.com>
2025-05-19 08:34:22 +0530

    UAT done reverse and Occurance

--- src/org/login/BaseClass.java
+++ src/org/login/BaseClass.java
@@ -1,16 +1,43 @@
 package org.login;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BaseClass {
-	
-	 void employeeId() {
+
+	void employeeId() {
 		System.out.println("employee id is 1234");
 	}
-	
+
+	public String reverseOFString(String input) {
+		String output = "";
+
+		for (int i = input.length() - 1; i >= 0; i--) {
+			char c = input.charAt(i);
+			output = output + c;
+		}
+
+		return output;
+	}
+
+	public Map<Character, Integer> occuranceCount(String input) {
+		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+
+		}
+		return map;
+	}
+
 	public static void main(String[] args) {
 		BaseClass a = new BaseClass();
 		a.employeeId();
-		
+
 	}
-	
-	
+
 }
